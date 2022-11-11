971b65b35f6094eafa9801f4c007126c4f0f2afd
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-11-11 21:13:58 +1100

    🎉 Update

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,10 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 
 def scrape(user: str, page: int):
-    pass
+    url = "https://myanimelist.net/animelist/Armaterasu"
+
+    out = requests.get(url)
+    soup = BeautifulSoup(out.text, features="html.parser")
+
+    tag = soup.find_all("table")
+    print(tag[0])
 
 
 if __name__ == "__main__":
-    pass
+    scrape(None, None)
