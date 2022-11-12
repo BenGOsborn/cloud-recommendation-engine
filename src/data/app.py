778c5986ac3cf1dcc8f6dd1bb070ed924ba6f1cf
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-11-12 11:32:34 +1100

    🎉 Update

--- src/data/app.py
+++ src/data/app.py
@@ -4,15 +4,14 @@ import utils
 
 
 def lambda_handler(event, context):
-    dynamodb = boto3.client("dynamodb")
-    table_name = "ratingsTable"
+    client = boto3.resource("dynamodb")
+    table = client.Table("ratingsTable")
 
     for record in event["Records"]:
         body = record["body"]
         user = body["user"]
 
         data = utils.scrape(user)
-        dynamodb.put_item(
-            TableName=table_name,
-            Item={"userId": {"S": json.dumps(data)}}
+        table.put_item(
+            Item={"userId": user, "data": json.dumps(data)}
         )
