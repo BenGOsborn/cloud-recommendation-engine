53664fe9dc6a1b582ca42d9b5f084b815531c6e2
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-11-12 12:23:31 +1100

    🎉 Update

--- src/model/app.py
+++ src/model/app.py
@@ -2,8 +2,16 @@ import torch
 
 
 class MatrixFactorization(torch.nn.Module):
-    def __init__(self, weights1, weights2):
+    def __init__(self, weights1, biases1, weights2, biases2):
         super().__init__()
 
+        # **** Remember, it will have to do multiple of the weights and biases at the same time - make sure it can compensate for this (that is one weights tensor = multiple users / shows)
+
         self.weights1 = weights1
+        self.biases1 = biases1
+
         self.weights2 = weights2
+        self.biases2 = biases2
+
+    def forward(self):
+        pass
