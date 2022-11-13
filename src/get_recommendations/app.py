3cbd749a06b775216dee626421e4cc5ee2d65178
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-11-13 22:56:27 +1100

    🎉 Update

--- src/get_recommendations/app.py
+++ src/get_recommendations/app.py
@@ -3,14 +3,12 @@ import json
 
 
 def lambda_handler(event, context):
-    body = json.loads(event["body"])
-
     client = boto3.resource("dynamodb")
 
     recommendations_table = client.Table("recommendationsTable")
 
     # Get the recommendations for the user
-    user = body["user"]
+    user = event["queryStringParameters"]["userId"]
 
     recommendations = recommendations_table.get_item(Key={
         "userId": user
