9f9776deb6dea975ca81c42e4e808dc0b0c42f54
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-11-13 21:56:17 +1100

    🎉 Update

--- src/model/train.py
+++ src/model/train.py
@@ -10,20 +10,27 @@ def lambda_handler(event, context):
     biases1_raw = body["biases1"]
     weights2_raw = body["weights2"]
     biases2_raw = body["biases2"]
+    target_raw = body["target"]
+    mask_raw = body["mask"]
 
     # Convert raw params to tensors
-    weights1, biases1, weights2, biases2 = [
+    weights1, biases1, weights2, biases2, target, mask = [
         utils.to_tensor(raw_tensor, False) for raw_tensor in [
             weights1_raw,
             biases1_raw,
             weights2_raw,
             biases2_raw,
+            target_raw,
+            mask_raw,
         ]
     ]
 
-    # Make predictions
-    predictions = utils.model(weights1, biases1, weights2, biases2)
+    # Train model
+    utils.fit(weights1, biases1, weights2, biases2, target, mask)
 
     return {
-        "predictions": json.dumps(predictions.tolist())
+        "weights1": weights1.tolist(),
+        "biases1": biases1.tolist(),
+        "weights2": weights2.tolist(),
+        "biases2": biases2.tolist(),
     }
