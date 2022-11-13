716d7dc44dde6c32dac76a21555612edaea8fa3e
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-11-13 15:35:11 +1100

    🎉 Update

--- src/model/app.py
+++ src/model/app.py
@@ -1,6 +1,42 @@
 import utils
+import json
 
 
 def lambda_handler(event, context):
+    # Load weights and biases from the request
+    body = json.loads(event["body"])
 
-    pass
+    weights1_raw = body["weights1"]
+    biases1_raw = body["biases1"]
+    weights2_raw = body["weights2"]
+    biases2_raw = body["biases2"]
+
+    weights1, biases1, weights2, biases2 = utils.transform_data(
+        weights1_raw,
+        biases1_raw,
+        weights2_raw,
+        biases2_raw
+    )
+
+    # Make predictions
+    predictions = utils.model(weights1, biases1, weights2, biases2)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(predictions.tolist())
+    }
+
+
+if __name__ == "__main__":
+    event = {
+        "body": json.dumps({
+            "weights1": [[1, 0]],
+            "biases1": [[0]],
+            "weights2": [[0, 1]],
+            "biases2": [[0]],
+        })
+    }
+
+    predictions = lambda_handler(event, None)
+
+    print(predictions)
--- src/model/utils.py
+++ src/model/utils.py
@@ -10,7 +10,7 @@ class MatrixFactorization(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    # Weights = n * x, Biases = n * 1
+    # Weights = x * y * 1, Biases = x * 1 * 1
     def forward(self, weights1, biases1, weights2, biases2):
         weights1 = torch.transpose(weights1, 1, 2)
         pred = torch.matmul(weights1, weights2)
@@ -70,16 +70,32 @@ if __name__ == "__main__":
 
 # Transform input weights and biases to Pytorch tensors
 def transform_data(weights1: List[List[float]], biases1: List[List[float]], weights2: List[List[float]], biases2: List[List[float]]):
-    weights1_tensor = torch.tensor(weights1, requires_grad=True)
+    weights1_tensor = torch.tensor(
+        weights1,
+        dtype=torch.float32,
+        requires_grad=True
+    )
     torch.reshape(weights1_tensor, (len(weights1), -1, 1))
 
-    weights2_tensor = torch.tensor(weights2, requires_grad=True)
+    weights2_tensor = torch.tensor(
+        weights2,
+        dtype=torch.float32,
+        requires_grad=True
+    )
     torch.reshape(weights2_tensor, (len(weights2), -1, 1))
 
-    biases1_tensor = torch.tensor(biases1, requires_grad=True)
+    biases1_tensor = torch.tensor(
+        biases1,
+        dtype=torch.float32,
+        requires_grad=True
+    )
     torch.reshape(biases1_tensor, (len(biases1), 1, 1))
 
-    biases2_tensor = torch.tensor(biases2, requires_grad=True)
+    biases2_tensor = torch.tensor(
+        biases2,
+        dtype=torch.float32,
+        requires_grad=True
+    )
     torch.reshape(biases2_tensor, (len(biases2), 1, 1))
 
     return weights1_tensor, biases1_tensor, weights2_tensor, biases2_tensor
