60487bb508c4a373608d3225a8bcac406a70e4dd
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-11-13 13:05:11 +1100

    🎉 Update

---
+++ src/model/entrypoint.py
@@ -0,0 +1,7 @@
+from sagemaker_inference import model_server
+
+
+if __name__ == "__main__":
+    model_server.start_model_server(
+        handler_service="/home/model-server/handler.py:handle"
+    )
--- src/model/handler.py
+++ src/model/handler.py
@@ -1,68 +1,14 @@
-import torch
-
-torch.manual_seed(1234)
+from sagemaker_inference.default_handler_service import DefaultHandlerService
 
 
-class MatrixFactorization(torch.nn.Module):
+class HandlerService(DefaultHandlerService):
     def __init__(self):
         super().__init__()
 
-    # Weights = n * x, Biases = n * 1
-    def forward(self, weights1, biases1, weights2, biases2):
-        pred = weights1 * weights2
-        pred = torch.sum(pred, dim=1, keepdim=True)
-        pred = pred + biases1 + biases2
+    # Initialize handler
+    def initialize(self, context):
+        return super().initialize(context)
 
-        return torch.relu(pred)
-
-
-n = 3
-x = 10
-
-
-weights1 = torch.rand(n, x, requires_grad=True)
-weights2 = torch.rand(n, x, requires_grad=True)
-
-biases1 = torch.rand(n, 1, requires_grad=True)
-biases2 = torch.rand(n, 1, requires_grad=True)
-
-target = torch.tensor([[1]] * n, dtype=torch.float32)
-
-print("Weights 1", weights1)
-print("Biases 1", biases1)
-
-print("Weights 2", weights2)
-print("Biases 2", biases2)
-
-print("Target", target)
-
-learning_rate = 0.007
-
-model = MatrixFactorization()
-loss = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(
-    [weights1, biases1, weights2, biases2],
-    lr=learning_rate
-)
-
-epochs = 1000
-
-for epoch in range(epochs):
-    prediction = model(weights1, biases1, weights2, biases2)
-
-    l = loss(prediction, target)
-
-    l.backward()
-
-    optimizer.step()
-    optimizer.zero_grad()
-
-
-prediction = model(weights1, biases1, weights2, biases2)
-print("Prediction", prediction)
-
-print("NEW Weights 1", weights1)
-print("NEW Biases 1", biases1)
-
-print("NEW Weights 2", weights2)
-print("NEW Biases 2", biases2)
+    # Handle an incoming request
+    def handle(self, data, context):
+        return super().handle(data, context)
