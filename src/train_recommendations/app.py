745e23f634a88839a77e4391714dd5217583287a
Your Name <you@example.com>
2022-11-16 21:59:53 +1100

    🎉 Update

--- src/train_recommendations/app.py
+++ src/train_recommendations/app.py
@@ -34,77 +34,24 @@ def lambda_handler(event, context):
         shows_params_table_name
     )
 
-    # get the trained params
-
     # Get the params
-    weights1 = [json.loads(params["weights"]) for params in user_params]
-    biases1 = [float(params["biases"]) for params in user_params]
-    weights2 = [json.loads(params["weights"]) for params in show_params]
-    biases2 = [float(params["biases"]) for params in show_params]
-
-    # Create training outputs and mask
-    target = []
-    mask = []
-
-    for i in range(len(user_params)):
-        temp_target = []
-        temp_mask = []
-
-        for j in range(len(show_params)):
-            show_id = shows_freq_list[j][0]
-
-            if i not in show_freq[show_id]:
-                temp_target.append(0)
-                temp_mask.append(1)
-            else:
-                temp_target.append(show_freq[show_id][i] / 10)
-                temp_mask.append(0)
-
-        target.append(temp_target)
-        mask.append(temp_mask)
-
-    # Train the params
-    results = lambda_client.invoke(
-        FunctionName="CloudRecommendationStack-trainModelFunction579FF5A-KtqZ0LNXgDmc",
-        InvocationType="RequestResponse",
-        Payload=json.dumps({
-            "body": json.dumps({
-                "weights1": weights1,
-                "biases1": biases1,
-                "weights2": weights2,
-                "biases2": biases2,
-                "target": target,
-                "mask": mask
-            })
-        })
+    new_weights1, new_biases1, new_weights2, new_biases2 = utils.train_params(
+        user_params,
+        show_params,
+        shows_freq,
+        shows_freq_list,
+        lambda_client,
+        train_model_function_name
     )
 
-    new_params = json.loads(
-        results["Payload"].read().decode("utf-8")
+    # Save updated params
+    utils.save_params(
+        users,
+        shows_freq_list,
+        new_weights1,
+        new_biases1,
+        new_weights2,
+        new_biases2,
+        users_params_table,
+        shows_params_table
     )
-
-    new_weights1 = new_params["weights1"]
-    new_biases1 = new_params["biases1"]
-    new_weights2 = new_params["weights2"]
-    new_biases2 = new_params["biases2"]
-
-    # Update the params with the new params
-    with users_params_table.batch_writer() as writer:
-        for i in range(len(user_params)):
-            writer.put_item(
-                Item={
-                    "userId": users[i]["userId"],
-                    "weights": json.dumps(new_weights1[i]),
-                    "biases": str(new_biases1[i]),
-                })
-
-    with shows_params_table() as writer:
-        for i in range(len(show_params)):
-            writer.put_item(
-                Item={
-                    "showId": shows_freq_list[i][0],
-                    "weights": json.dumps(new_weights2[i]),
-                    "biases": str(new_biases2[i]),
-                    "cluster": show_params[i]["cluster"],
-                }
-            )
--- src/train_recommendations/utils.py
+++ src/train_recommendations/utils.py
@@ -59,3 +59,98 @@ def fetch_data(users: List, shows_freq_list: List, db_client: any, users_params_
     show_params = batch["showsParamsTable"]
 
     return user_params, show_params
+
+
+def train_params(
+    user_params: List,
+    show_params: List,
+    shows_freq: any,
+    shows_freq_list: List,
+    lambda_client: any,
+    train_model_function_name: str
+):
+    # Get the params
+    weights1 = [json.loads(params["weights"]) for params in user_params]
+    biases1 = [float(params["biases"]) for params in user_params]
+    weights2 = [json.loads(params["weights"]) for params in show_params]
+    biases2 = [float(params["biases"]) for params in show_params]
+
+    # Create training outputs and mask
+    target = []
+    mask = []
+
+    for i in range(len(user_params)):
+        temp_target = []
+        temp_mask = []
+
+        for j in range(len(show_params)):
+            show_id = shows_freq_list[j][0]
+
+            if i not in shows_freq[show_id]:
+                temp_target.append(0)
+                temp_mask.append(1)
+            else:
+                temp_target.append(shows_freq[show_id][i] / 10)
+                temp_mask.append(0)
+
+        target.append(temp_target)
+        mask.append(temp_mask)
+
+    # Train the params
+    results = lambda_client.invoke(
+        FunctionName=train_model_function_name,
+        InvocationType="RequestResponse",
+        Payload=json.dumps({
+            "body": json.dumps({
+                "weights1": weights1,
+                "biases1": biases1,
+                "weights2": weights2,
+                "biases2": biases2,
+                "target": target,
+                "mask": mask
+            })
+        })
+    )
+
+    new_params = json.loads(
+        results["Payload"].read().decode("utf-8")
+    )
+
+    new_weights1 = new_params["weights1"]
+    new_biases1 = new_params["biases1"]
+    new_weights2 = new_params["weights2"]
+    new_biases2 = new_params["biases2"]
+
+    return new_weights1, new_biases1, new_weights2, new_biases2
+
+
+# Save the updated params
+def save_params(
+    users: List,
+    shows_freq_list: List,
+    weights1: List,
+    biases1: List,
+    weights2: List,
+    biases2: List,
+    users_params_table: any,
+    shows_params_table: any
+):
+    # Update the params with the new params
+    with users_params_table.batch_writer() as writer:
+        for i in range(len(weights1)):
+            writer.put_item(
+                Item={
+                    "userId": users[i]["userId"],
+                    "weights": json.dumps(weights1[i]),
+                    "biases": str(biases1[i]),
+                })
+
+    with shows_params_table() as writer:
+        for i in range(len(weights2)):
+            writer.put_item(
+                Item={
+                    "showId": shows_freq_list[i][0],
+                    "weights": json.dumps(weights2[i]),
+                    "biases": str(biases2[i]),
+                }
+            )
